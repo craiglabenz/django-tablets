41782a253030179520b5258156ff8ca247abcc38
Scott Anderson <scottanderson42@gmail.com>
2015-12-08 13:37:24 -0500

    Changes Jinja2 loader to use standard JINJA2_LOADER_SETTINGS. - moves 'JINJA2_SHOULD_RELOAD_DB_TEMPLATES' to 'should_reload_db_templates' in JINJA2_LOADER_SETTINGS
    Adds 'should_prioritize_filesystem' to allow the filesystem fallback to occur first if desired.
    
    Fixes requirements.txt to pin working Django version at 1.6 and working django_jinja2 version at 1.0.1.
    
    Moves j2/loader.py:Jinja2DatabaseOrFileLoader.uptodate to class level, as it doesn't need be a closure.

--- tablets/j2/loaders.py
+++ tablets/j2/loaders.py
@@ -7,6 +7,8 @@ from django.template.loader import BaseLoader
 from django.template.loaders import app_directories
 from django.utils import six
 
+from jinja2.exceptions import TemplateNotFound
+
 default_loader_dirs = (app_directories.app_template_dirs +
                        tuple(settings.TEMPLATE_DIRS))
 
@@ -31,28 +33,49 @@ class Jinja2DatabaseOrFileLoader(FileSystemLoader):
     """
     This guy talks to the database and returns something Jinja2 wants.
     """
-    def __init__(self, searchpath=None, encoding='utf-8'):
+    def __init__(self, searchpath=None, encoding='utf-8', should_reload_db_templates=None, should_prioritize_filesystem=False):
         searchpath = searchpath or default_loader_dirs
         if isinstance(searchpath, six.string_types):
             searchpath = [searchpath]
         self.searchpath = list(searchpath)
         self.encoding = encoding
 
-    def get_source(self, environment, template):
+        if should_reload_db_templates is None:
+            # Backwards compatibility: if the parameter is not passed in via JINJA2_LOADER_SETTINGS,
+            # look for it using the old settings value.
+            should_reload_db_templates = getattr(settings, 'JINJA2_SHOULD_RELOAD_DB_TEMPLATES', True)
+
+        self.should_reload_db_templates = should_reload_db_templates
+        self.should_prioritize_filesystem = should_prioritize_filesystem
+
+    def uptodate(self):
+        return not self.should_reload_db_templates
+
+    def _load_from_database(self, environment, template):
         # Local Apps
         from tablets.models import Template
 
+        try:
+            tmpl = Template.objects.get(name=template, template_engine=Template.JINJA2)
+            return (tmpl.get_content().decode(self.encoding), template, self.uptodate)
+        except Template.DoesNotExist:
+            raise TemplateNotFound(template)
+
+    def get_source(self, environment, template):
         if not jinja2:
             raise Jinja2NotInstalled
 
-        def uptodate():
-            return not getattr(settings, "JINJA2_SHOULD_RELOAD_DB_TEMPLATES", True)
+        if self.should_prioritize_filesystem:
+            try:
+                return super(Jinja2DatabaseOrFileLoader, self).get_source(environment, template)
+            except TemplateNotFound:
+                return self._load_from_database(environment, template)
 
-        try:
-            tmpl = Template.objects.get(name=template, template_engine=Template.JINJA2)
-            return tmpl.get_content().decode(self.encoding), template, uptodate
-        except Template.DoesNotExist:
-            return super(Jinja2DatabaseOrFileLoader, self).get_source(environment, template)
+        else:
+            try:
+                return self._load_from_database(environment, template)
+            except TemplateNotFound:
+                return super(Jinja2DatabaseOrFileLoader, self).get_source(environment, template)
 
 
 class DjangoJinja2DatabaseLoader(BaseLoader):
