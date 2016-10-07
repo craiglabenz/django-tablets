c1fe5e2e806dfb20e5d883edf93c45b0f2324fc6
Craig Labenz <craig.labenz@gmail.com>
2016-10-07 14:36:46 -0400

    Scrubs history of `django-annoying` ever existing
    Bumps to 0.6.0

--- tablets/migrations/0001_initial.py
+++ tablets/migrations/0001_initial.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import annoying.fields
+import jsonfield.fields
 
 
 class Migration(migrations.Migration):
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=255)),
                 ('content', models.TextField(blank=True)),
                 ('template_engine', models.IntegerField(default=1, verbose_name='Template Engine', choices=[(1, 'Django'), (2, 'Jinja2')])),
-                ('default_context', annoying.fields.JSONField(default='{}', verbose_name='Default Context', blank=True)),
+                ('default_context', jsonfield.fields.JSONField(blank=True, default=dict, help_text='Does not work so well for Jinja2 templates, which throw exceptions for missing values. This can make things tough if your template relies on functions.', verbose_name='Default Context')),
                 ('parent', models.ForeignKey(related_name='children', blank=True, to='tablets.Template', help_text='Select another template this template should extend.', null=True)),
             ],
             options={
