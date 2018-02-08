8545620c5cc418aa010b91cbaa6d3876a55564eb
Craig Labenz <craig.labenz@gmail.com>
2018-02-08 15:29:18 -0500

    Bumped version to get missing __init__ file into pypi

--- tablets/__init__.py
+++ tablets/__init__.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 """Package for Tablets"""
 
 __project__ = "Tablets"
-__version__ = "0.6.5rc4"
+__version__ = "0.6.5rc5"
 
 VERSION = __project__ + '-' + __version__
