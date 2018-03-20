22befb9ce8a0fed3b9d034a4c874b738f622e888
Craig Labenz <craig.labenz@gmail.com>
2018-03-20 13:25:44 -0400

    Moved code from previous commit to correct place

--- tablets/loaders/database.py
+++ tablets/loaders/database.py
@@ -35,3 +35,14 @@ class Loader(Loader):
     def get_contents(self, origin):
         template, name = self.load_template(origin.template_name)
         return template.source
+
+    def get_template(self, template_name, template_dirs=None, skip=None):
+        template = super(Loader, self).get_template(template_name, template_dirs, skip)
+
+        # TODO: Create a new Engine that is does nothing special outside of
+        #       the default Engine, except for its ability to programmatically
+        #       determine which Tablet was used
+        if not hasattr(template, 'tablet'):
+            template.tablet = Template.objects.get(name=template_name)
+
+        return template
